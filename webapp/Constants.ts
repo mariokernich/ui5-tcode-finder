4db621bad9fdc47b99cc095be57556587237ef34
Mario Kernich <mario.kernich@swan.de>
2025-03-19 00:01:29 +0100

    feat: update GitHub URL in constants to reflect new repository location

--- webapp/Constants.ts
+++ webapp/Constants.ts
@@ -1,5 +1,5 @@
 export default {
-	GITHUB_URL: "https://github.com/marioke/de.kernich.tcode",
+	GITHUB_URL: "https://github.com/mariokernich/ui5-tcode-finder",
 	LINKEDIN_URL: "https://www.linkedin.com/in/mario-kernich",
 	TCODE_GROUPS: ["GENERAL", "UI5", "ABAP", "EWM", "ERP", "FI", "CUSTOM"],
 };
